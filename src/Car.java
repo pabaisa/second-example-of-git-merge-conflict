f015a845376fc79202e942e1cb6ad25b2e3cae5a
Modestas <modestasbabonas@gmail.com>
2019-12-07 10:55:44 +0200

    add year

--- src/Car.java
+++ src/Car.java
@@ -1,24 +1,18 @@
 public class Car {
 
     private String model;
-    private double price;
     private int year;
 
     public Car(int year) {
         this.year = year;
     }
 
-    public Car(double price) {
-        this.price = price;
-    }
 
     public Car(String model) {
         this.model = model;
     }
 
-    public double getPrice() {
-        return price;
-    }
+
 
     public int getYear() {
         return year;
